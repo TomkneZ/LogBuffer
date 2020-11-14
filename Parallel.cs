596ca00b702ed8f845425a7934548e05ce774c45
ваше имя <zabenkotoi@gmail.com>
2020-11-14 16:55:19 +0300

    all done

---
+++ Parallel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Parallel
+    {        
+        public static void WaitAll(Action[] delegates)
+        {
+            List<Task> tasks = new List<Task>();
+            Task task;
+            foreach (Action del in delegates)
+            {
+                task = new Task(del);
+                tasks.Add(task);
+                task.Start();               
+            }
+            Task.WaitAll(tasks.ToArray());            
+        }                
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало Main");
+            Action[] delegates = new Action[] { MyAction, MyAction, MyAction, MyAction, MyAction };
+            Parallel.WaitAll(delegates);
+            Console.WriteLine("Конец Main");
+            Console.ReadLine();
+        }
+
+        public static void MyAction()
+        {
+            var threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Сейчас работает поток {threadId}");
+            Thread.Sleep(3000);
+        }
+    }
+}
